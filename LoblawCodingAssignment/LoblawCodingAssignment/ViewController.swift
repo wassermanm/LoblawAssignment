1217d277adc7f3bc005d68f98c7a7abc25759025
michael wasserman <michaelwasserman@MacBook-Pro.local>
2020-08-05 18:50:26 -0400

    Initial project commit

---
+++ LoblawCodingAssignment/LoblawCodingAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoblawCodingAssignment
+//
+//  Created by michael wasserman on 2020-08-05.
+//  Copyright © 2020 michael wasserman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
